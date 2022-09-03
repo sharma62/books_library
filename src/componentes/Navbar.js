e67897446e945c9d61387a14eaa931a576242136
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-03 23:59:46 +0530

    a

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar(props) {
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              option
             </a>
             <ul className="dropdown-menu">
               <li><a className="dropdown-item" href="/">Action</a></li>
