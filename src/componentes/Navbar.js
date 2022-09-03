3c35c227ceb0b11518f321fcfbaffdf461b926f6
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-03 23:59:02 +0530

    update nav

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -25,10 +25,9 @@ export default function Navbar(props) {
               <li><a className="dropdown-item" href="/">Action</a></li>
               <li><a className="dropdown-item" href="/">Another action</a></li>
               <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="/">Something else here</a></li>
+              <li><a className="dropdown-item" href="/">Something search</a></li>
             </ul>
           </li>
-         
         </ul>
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
