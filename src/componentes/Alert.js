820765a698ecb7b6cdd85f1b7a3f90d8ad84862a
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-04 23:53:41 +0530

    Add Alert compnent

---
+++ src/componentes/Alert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Alert() {
+    return (
+        <>
+            <div className="container">
+                <div class="alert alert-primary" role="alert">
+                    A simple primary alert—check it out!
+                </div>
+            </div>
+        </>
+    )
+}
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -30,7 +30,7 @@ export default function Navbar(props) {
           </li>
         </ul>
         <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <inp ut className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
