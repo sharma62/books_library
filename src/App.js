e6998d14e8cfbd2b1402ac3228cae121c57358c8
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-07 20:55:22 +0530

    update ItemCard

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Alert from './componentes/Alert';
       <Jumbotron  title="Dive in Store" heading ="Welcome To Library" helperText = "Here you can connect with library"  moreInfo= "Comming soon" actionSrc="/"/>
       <div className="container-fluid" >
         <h2 className='text-center py-4'>Books</h2>
-        <div className=" my-4 d-flex " style={{overflow: 'scroll'}}>
+        <div className=" my-4 d-flex" style={{overflow: 'scroll'}}>
           <ItemCard title="C++" about="Good Explanation" imgSrc="https://i.postimg.cc/wM0QGsxg/Cpp.jpg" readSrc="https://www.w3schools.com/cpp/cpp_intro.asp#:~:text=C%2B%2B%20is%20one%20of%20the,be%20reused%2C%20lowering%20development%20costs." />
           <ItemCard title="Opreating System" about="Good Explanation" imgSrc="https://i.postimg.cc/9fLXzmfz/OS.jpg" readSrc="https://www.tutorialspoint.com/operating_system/os_overview.htm" />
           <ItemCard title="python" about="Good Explanation" imgSrc="https://i.postimg.cc/vmh9YSdh/python.jpg" readSrc="https://www.python.org/" />
